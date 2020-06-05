24781735e313bd1017df1b87c045436eea0d18f6
Lee JungHyeok <wndgur76@naver.com>
2020-06-05 22:51:29 +0900

    Log in module for apm
    Used testAscii for make pattern to hide each character in string(id/pw).

--- login.py
+++ login.py
@@ -1,3 +1,5 @@
+import random
+
 
 def login():
 
@@ -27,25 +29,25 @@ def login():
         id_pwList[i][1] = id_pwList[i][1][0:len(id_pwList[i][1])-1]
     
     while not permission:
-        print(id_pwList)
         fileId.seek(0)
         idFound = False
         password = 'a'
         identity = input("\n---로그인---\n아이디를 입력하세요.\n\n")
+        identityHidden = hide(identity)
         for i in id_pwList:
-            if identity ==i[0]:
+            if identityHidden ==i[0]:
                 idFound = True
-                pwFound = i[1]
+                pwHidden = i[1]
 
         if idFound == False:
             print("\n존재하지 않는 아이디입니다.\n아이디를 생성하시겠습니까?")
             createId = input("(Y/N)\n").upper()
             if createId == 'Y':
                 newId = input("\n새로운 아이디를 입력하세요.\n")
-                
+                newIdHidden = hide(newId)
                 existId = False
                 for i in id_pwList:
-                    if i[0] == newId:
+                    if i[0] == newIdHidden:
                         existId = True
                         print("\n이미 존재하는 아이디입니다.\n로그인으로 돌아갑니다.\n")
                         break
@@ -58,9 +60,9 @@ def login():
                     newPw2 = input("다시 한번 입력하세요.\n")
                     if newPw == newPw2:
                         fileId.readlines()
-                        fileId.write(newId)
+                        fileId.write(newIdHidden)
                         fileId.write(':')
-                        fileId.write(newPw)
+                        fileId.write(hide(newPw))
                         fileId.write('\n')
                         fileId.close()
                         print("\n아이디가 생성되었습니다.\n다시 로그인해주세요.")
@@ -70,7 +72,7 @@ def login():
                         id_pwList = []
                         for i in range(len(idpwList)):
                             id_pwList.append(idpwList[i].split(':'))
-                        for i in range(len(idpwList)-1):
+                        for i in range(len(idpwList)):
                             id_pwList[i][1] = id_pwList[i][1][0:len(id_pwList[i][1])-1]
                         break
                     else:
@@ -78,7 +80,7 @@ def login():
         else:
             while (not permission) and (password.upper() != 'B'):
                 password = input("\n비밀번호를 입력하세요.\n\n")
-                if password == pwFound:
+                if password == decode(pwHidden):
                     fileId.close()
                     print("\n로그인 되었습니다.")
                     permission = True
@@ -90,4 +92,21 @@ def login():
     
     return permission
 
-print(login())
+
+arrKeys = []
+for i in range(33, 127):
+    arrKeys.append(chr(i))
+
+arrFakeKeys = ['(', 'X', 'm', 'x', 'Q', 'F', 'f', ')', '7', 'c', 'K', ',', 'd', '8', '/', 'g', '[', 's', '{', 'n', '^', '+', '@', '1', 'H', '?', '.', '~', '<', 'E', 'O', ':', 'V', 'a', 'Z', '$', 'W', 'y', '|', '\\', "'", 'B', '5', '6', 'r', 'z', 'Y', 'L', 'b', '%', 'o', '&', 'R', '4', 't', 'v', '9', 'u', 'h', 'G', '!', 'T', 'N', '*', 'k', 'C', '#', 'M', 'i', 'D', 'J', '=', 'U', 'e', '`', '>', 'w', 'p', '-', 'l', 'I', '2', 'A', '_', 'q', 'S', '}', ';', '"', 'j', '0', ']', 'P', '3']
+# made by ascii hiding system
+def hide(string):
+    newString = ""
+    for ch in string:
+        newString += arrFakeKeys[arrKeys.index(ch)]
+    return newString
+
+def decode(string):
+    stringDecoded = ""
+    for ch in string:
+        stringDecoded += arrKeys[arrFakeKeys.index(ch)]
+    return stringDecoded
